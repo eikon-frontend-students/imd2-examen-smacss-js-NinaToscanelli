a2b2d2b3ad2da7698c262127045cd7c283c44d03
ninatoscanelli <nina.toscanelli@studentfr.ch>
2025-02-06 17:12:53 +0100

    fini

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,19 @@ function toggleColor() {
 }
 
 header.addEventListener("click", toggleColor);
+
+var h1 = document.querySelector(".toggle-#005564");
+
+function toggleColor() {
+  h1.classList.toggle("is-#005564");
+}
+
+h1.addEventListener("click", toggleColor);
+
+var block = document.querySelector(".toggle-#CF9697");
+
+function toggleColor() {
+  block.classList.toggle("is-#CF9697");
+}
+
+block.addEventListener("click", toggleColor);
